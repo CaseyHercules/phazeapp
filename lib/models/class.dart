9c3897fd0b90a7c696531ff9a7409ae74c3f67b8
Casey Holman <kchholman@gmail.com>
2022-05-29 16:33:32 -0500

    downgrade flutter

--- lib/models/class.dart
+++ lib/models/class.dart
@@ -1,6 +1,5 @@
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'dart:convert';
